2eea2333cb782ec5e64f912e3117b929af8529c0
5201314_Zq <ya942371102@qq.com>
2022-04-05 04:02:54 +0800

    添加头像

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,6 +5,17 @@
             <i class="el-icon-menu"></i>
 
         </div>
+        <div class="logo">music后台管理</div>
+        <div class="header-right">
+            <div class="btn-fullscreen" @click="handleFullScreen">
+                <el-tooltip :content="fullscreen?`取消全屏`:`全屏`" placement="bottom">
+                    <i class="el-icon-rank"></i>
+                </el-tooltip>
+            </div>
+            <div class="user-avator">
+                <img src="../assets/img/user.jpg" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,13 +24,47 @@ import bus from "../assets/js/bus";
 export default {
     data() {
         return{
-            collapse: false
+            collapse: false,
+            fullscreen: false
         }
     },
     methods:{
+        //侧边栏
         collapseChage(){
             this.collapse = !this.collapse;
             bus.$emit('collapse', this.collapse);
+        },
+        //全屏事件
+        handleFullScreen(){
+            if(this.fullscreen){
+                if(document.exitFullscreen){
+                    document.exitFullscreen()
+                }else if(document.webkitCancelFullScreen){
+                    // safari、chrom
+                    document.webkitCancelFullScreen();
+                }else if(document.mozCancelFullScreen){
+                    // firefox
+                    document.mozCancelFullScreen();
+                }else if(document.msExitFullScreeen){
+                    // ie
+                    document.msExitFullScreeen();
+                }
+            }else{
+                let element = document.documentElement;
+                if(element.requestFullscreen){
+                    element.requestFullscreen()
+                }else if(element.webkitRequestFullScreen){
+                    // safari、chrom
+                    element.webkitRequestFullScreen();
+                }else if(element.mozRequestFullScreen){
+                    // firefox
+                    element.mozRequestFullScreen();
+                }else if(element.msRequestFullScreeen){
+                    // ie
+                    element.msRequestFullScreeen();
+                }
+            }
+            this.fullscreen = !this.fullscreen;
         }
     }
 }
@@ -42,6 +87,37 @@ export default {
     padding: 0 21px;
     cursor: pointer;
     line-height: 70px; 
+}
+
+.header .logo{
+    position: relative;
+    float: left;
+    line-height: 70px;
+}
+
+.header-right{
+    float: right;
+    padding-right: 50px;
+    display: flex;
+    height: 70px;
+    align-items: center;
+}
+
+.btn-fullscreen{
+    transform: rotate(45deg);
+    margin-right: 5px;
+    font-size: 24px;
+}
+
+.user-avator{
+    margin-left: 20px;
+}
+
+.user-avator img{
+    display: block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
 }
 
 </style>
